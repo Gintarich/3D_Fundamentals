c87de1ea01c5317eefda176be05af089ab485835
Gintarich <71980448+Gintarich@users.noreply.github.com>
2022-03-11 23:26:17 +0200

    dsa

--- Engine/Game.cpp
+++ Engine/Game.cpp
@@ -22,11 +22,11 @@
 #include "Game.h"
 #include "Mat3.h"
 
-Game::Game( MainWindow& wnd )
+Game::Game(MainWindow& wnd)
 	:
-	wnd( wnd ),
-	gfx( wnd ),
-	cube( 1.0f )
+	wnd(wnd),
+	gfx(wnd),
+	cube(1.0f)
 {
 }
 
@@ -41,29 +41,29 @@ void Game::Go()
 void Game::UpdateModel()
 {
 	const float dt = 1.0f / 60.0f;
-	if( wnd.kbd.KeyIsPressed( 'Q' ) )
+	if( wnd.kbd.KeyIsPressed('Q') )
 	{
-		theta_x = wrap_angle( theta_x + dTheta * dt );
+		theta_x = wrap_angle(theta_x + dTheta * dt);
 	}
-	if( wnd.kbd.KeyIsPressed( 'W' ) )
+	if( wnd.kbd.KeyIsPressed('W') )
 	{
-		theta_y = wrap_angle( theta_y + dTheta * dt );
+		theta_y = wrap_angle(theta_y + dTheta * dt);
 	}
-	if( wnd.kbd.KeyIsPressed( 'E' ) )
+	if( wnd.kbd.KeyIsPressed('E') )
 	{
-		theta_z = wrap_angle( theta_z + dTheta * dt );
+		theta_z = wrap_angle(theta_z + dTheta * dt);
 	}
-	if( wnd.kbd.KeyIsPressed( 'A' ) )
+	if( wnd.kbd.KeyIsPressed('A') )
 	{
-		theta_x = wrap_angle( theta_x - dTheta * dt );
+		theta_x = wrap_angle(theta_x - dTheta * dt);
 	}
-	if( wnd.kbd.KeyIsPressed( 'S' ) )
+	if( wnd.kbd.KeyIsPressed('S') )
 	{
-		theta_y = wrap_angle( theta_y - dTheta * dt );
+		theta_y = wrap_angle(theta_y - dTheta * dt);
 	}
-	if( wnd.kbd.KeyIsPressed( 'D' ) )
+	if( wnd.kbd.KeyIsPressed('D') )
 	{
-		theta_z = wrap_angle( theta_z - dTheta * dt );
+		theta_z = wrap_angle(theta_z - dTheta * dt);
 	}
 }
 
@@ -71,19 +71,19 @@ void Game::ComposeFrame()
 {
 	auto lines = cube.GetLines();
 	const Mat3 rot =
-		Mat3::RotationX( theta_x ) *
-		Mat3::RotationY( theta_y ) *
-		Mat3::RotationZ( theta_z );
+		Mat3::RotationX(theta_x) *
+		Mat3::RotationY(theta_y) *
+		Mat3::RotationZ(theta_z);
 	for( auto& v : lines.vertices )
 	{
 		v *= rot;
-		v += { 0.0f,0.0f,1.0f };
-		pst.Transform( v );
+		v += { 0.0f, 0.0f, 1.0f };
+		pst.Transform(v);
 	}
 	for( auto i = lines.indices.cbegin(),
 		end = lines.indices.cend();
-		i != end; std::advance( i,2 ) )
+		i != end; std::advance(i, 2) )
 	{
-		gfx.DrawLine( lines.vertices[*i],lines.vertices[*std::next( i )],Colors::White );
+		gfx.DrawLine(lines.vertices[ *i ], lines.vertices[ *std::next(i) ], Colors::White);
 	}
 }
